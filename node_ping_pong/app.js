e47b4e6c7e9ac0ae1c1567d5418271bfcbebe83d
David <dave.com>
2023-12-13 12:24:04 +0400

    Application ping pong result

---
+++ node_ping_pong/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/ping', (req, res) => {
+  res.send('pong\n');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
